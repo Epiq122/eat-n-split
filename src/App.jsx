bb268484558ec24aa3de087d1f4d96cfd422f39c
Robert Gleason <l2ob122@gmail.com>
2023-06-14 21:15:22 -0700

    added state to show the form when button is clicked

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
+import { useState } from 'react';
 import Button from './components/Button';
 import FormAddFriend from './components/FormAddFriend';
 import FormSplitBill from './components/FormSplitBill';
 import FriendsList from './components/FriendsList';
 
 function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+
+  const handleShowAddFriend = () => {
+    setShowAddFriend(!showAddFriend);
+  };
+
   return (
     <div className="app">
       <div className="sidebar">
         <FriendsList />
 
-        <FormAddFriend />
-        <Button>Add Friend</Button>
+        {showAddFriend && <FormAddFriend />}
+        <Button onClick={handleShowAddFriend}>
+          {showAddFriend ? 'Close' : 'Add Friend'}
+        </Button>
       </div>
       <FormSplitBill />
     </div>
